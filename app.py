4d36e751a942738605153013654ccf1e4af76813
grayvoyager <81471436+grayvoyager@users.noreply.github.com>
2022-08-30 16:31:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,81 @@
+import streamlit as st
+import pandas as pd
+import pickle
+import requests
+from PIL import Image
+image = Image.open('thoseclassics.jpg')
+st.image(image, caption=' ')
+st.title('Cine Recommender')
+movies_dict= pickle.load(open('movie_dict.pkl','rb'))
+movies= pd.DataFrame(movies_dict)
+similarity= pickle.load(open('similarity.pkl','rb'))
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
+def recommend(movie):
+     movie_index = movies[movies['title'] == movie].index[0]
+     distances = similarity[movie_index]
+     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+     recommended_movies = []
+     recommended_movies_posters = []
+     for i in movies_list:
+          movie_id=movies.iloc[i[0]].movie_id
+          recommended_movies.append(movies.iloc[i[0]].title)
+          recommended_movies_posters.append(fetch_poster(movie_id))
+     return recommended_movies,recommended_movies_posters
+
+selected_movie_name = st.selectbox(
+     'Type in or select a movie from the dropdown below',
+     movies['title'].values)
+if st.button('Find similar'):
+    recommended_movie_names,recommended_movie_posters = recommend(selected_movie_name)
+    col1, col2, col3, col4, col5 = st.beta_columns(5)
+    with col1:
+        st.text(recommended_movie_names[0])
+        st.image(recommended_movie_posters[0])
+    with col2:
+        st.text(recommended_movie_names[1])
+        st.image(recommended_movie_posters[1])
+
+    with col3:
+        st.text(recommended_movie_names[2])
+        st.image(recommended_movie_posters[2])
+    with col4:
+        st.text(recommended_movie_names[3])
+        st.image(recommended_movie_posters[3])
+    with col5:
+        st.text(recommended_movie_names[4])
+        st.image(recommended_movie_posters[4])
+
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+color: white;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Developed with a pinch of  ❤  by a Targaryen</a></p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
+
